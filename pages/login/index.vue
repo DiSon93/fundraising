761f0d5b5fcec2712035bfe23eb177d2a94a5710
Nguyen Quang Tri <trinq@vutatech.vn>
2021-10-02 13:04:48 +0700

    login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -85,169 +85,173 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+  import { mapState, mapActions } from "vuex";
 
-export default {
-  data() {
-    return {
-      value: 0,
-      hasOpenLoading: false,
-      disabled: true,
-      loading: false,
-    };
-  },
-  beforeCreate() {
-    this.form = this.$form.createForm(this, { name: "login" });
-  },
-  mounted() {
-    this.handleClickLoading("points");
-    window.myRecaptchaMethod = this.myRecaptchaMethod;
-    window.myRecaptchaExpiredMethod = this.myRecaptchaExpiredMethod;
-  },
-  computed: {
-    ...mapState("auth", ["errorMessage"]),
-  },
-  methods: {
-    myRecaptchaMethod: function (response) {
-      console.log(response);
-      this.disabled = false;
+  export default {
+    data() {
+      return {
+        value: 0,
+        hasOpenLoading: false,
+        disabled: true,
+        loading: false,
+        recaptcha: ''
+      };
     },
-    myRecaptchaExpiredMethod: function () {
-      this.disabled = true;
+    beforeCreate() {
+      this.form = this.$form.createForm(this, { name: "login" });
+    },
+    mounted() {
+      this.handleClickLoading("points");
+      window.myRecaptchaMethod = this.myRecaptchaMethod;
+      window.myRecaptchaExpiredMethod = this.myRecaptchaExpiredMethod;
+    },
+    computed: {
+      ...mapState("auth", ["errorMessage"]),
+    },
+    methods: {
+      myRecaptchaMethod: function (response) {
+        // console.log(response);
+        this.recaptcha = response;
+        this.disabled = false;
+      },
+      myRecaptchaExpiredMethod: function () {
+        this.recaptcha = '';
+        this.disabled = true;
 
-      // this is google's code verifying the user is human'
-      // that you check with google on the backend.
-    },
-    handleSubmit(e) {
-      e.preventDefault();
-      this.form.validateFieldsAndScroll((err, values) => {
-        if (!err) {
-          console.log("Received values of form: ", values);
-          this.loginIntoServer(values);
-        }
-      });
-    },
-    handleClickLoading(type) {
-      const loading = this.$vs.loading({
-        type,
-        color: "#0b9985",
-      });
-      this.hasOpenLoading = true;
-      setTimeout(() => {
-        loading.close();
-        this.hasOpenLoading = false;
-      }, 1000);
-    },
-    async loginIntoServer(user) {
-      this.loading = true;
-      try {
-        await this.$store.dispatch("auth/loginIntoServer", {
-          email: user.email,
-          password: user.password,
+        // this is google's code verifying the user is human'
+        // that you check with google on the backend.
+      },
+      handleSubmit(e) {
+        e.preventDefault();
+        this.form.validateFieldsAndScroll((err, values) => {
+          if (!err) {
+            // console.log("Received values of form: ", values);
+            this.loginIntoServer(values);
+          }
         });
-        if (this.errorMessage) {
-          this.loading = false;
-          this.openNotification("top-right", "danger", "Error");
-        } else {
-          this.openNotification("top-right", "#0b9985", "Success");
-          this.loading = false;
-          this.$router.push("/dashboard");
+      },
+      handleClickLoading(type) {
+        const loading = this.$vs.loading({
+          type,
+          color: "#0b9985",
+        });
+        this.hasOpenLoading = true;
+        setTimeout(() => {
+          loading.close();
+          this.hasOpenLoading = false;
+        }, 1000);
+      },
+      async loginIntoServer(user) {
+        this.loading = true;
+        try {
+          await this.$store.dispatch("auth/loginIntoServer", {
+            email: user.email,
+            password: user.password,
+            recaptcha: this.recaptcha
+          });
+          if (this.errorMessage) {
+            this.loading = false;
+            this.openNotification("top-right", "danger", "Error");
+          } else {
+            this.openNotification("top-right", "#0b9985", "Success");
+            this.loading = false;
+            this.$router.push("/dashboard");
+          }
+        } catch {
+          console.log("Error", this.errorMessage);
         }
-      } catch {
-        console.log("Error", this.errorMessage);
-      }
+      },
+      openNotification(position = null, color, title) {
+        const noti = this.$vs.notification({
+          flat: true,
+          progress: "auto",
+          color,
+          position,
+          title,
+          text: this.errorMessage ? this.errorMessage : `Login success`,
+        });
+      },
     },
-    openNotification(position = null, color, title) {
-      const noti = this.$vs.notification({
-        flat: true,
-        progress: "auto",
-        color,
-        position,
-        title,
-        text: this.errorMessage ? this.errorMessage : `Login success`,
-      });
-    },
-  },
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "@asset/variables.scss";
-.login {
-  width: 50%;
-  background: $background-form;
-  border: 1px solid #e4e4e4;
-  box-sizing: border-box;
-  box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.09);
-  border-radius: 20px;
-  padding: 0px 140px;
-  margin: 0 auto;
-  margin-top: 20px;
-  .login_title {
-    font-weight: bold;
-    font-size: 36px;
-    line-height: 24px;
-    color: $green-black-01;
-    margin-top: 30px;
-    text-align: center;
-  }
-  .content {
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 24px;
-    margin-top: 10px;
-    margin-bottom: 30px;
-    text-align: center;
-  }
-}
-.ant-form {
-  input {
-    border-radius: 50px !important;
-  }
-  .ant-form-item {
-    margin-bottom: 12px;
-  }
-  .ant-input {
-    border-radius: 50px !important;
-    margin: -16px 0px !important;
-    height: 40px !important;
-    padding: 4px 20px;
-  }
-  .ant-form-item-children {
-    .ant-input {
-      border-radius: 50px !important;
+  @import "@asset/variables.scss";
+  .login {
+    width: 50%;
+    background: $background-form;
+    border: 1px solid #e4e4e4;
+    box-sizing: border-box;
+    box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.09);
+    border-radius: 20px;
+    padding: 0px 140px;
+    margin: 0 auto;
+    margin-top: 20px;
+    .login_title {
+      font-weight: bold;
+      font-size: 36px;
+      line-height: 24px;
+      color: $green-black-01;
+      margin-top: 30px;
+      text-align: center;
+    }
+    .content {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 24px;
+      margin-top: 10px;
+      margin-bottom: 30px;
+      text-align: center;
     }
   }
-  .remember_me {
-    margin-top: 0px;
-    font-size: 16px;
-    font-weight: 500;
-  }
-  .form_submit {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-  }
-  .btn_started {
-    background: linear-gradient(90.62deg, #0b9985 -63.29%, #0b9985 100%);
-    border-radius: 50px;
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 24px;
-    padding: 2px 12px;
-  }
-  .more_option {
-    margin-bottom: 30px;
-    .confirm,
-    .forgot {
-      font-weight: 500;
+  .ant-form {
+    input {
+      border-radius: 50px !important;
+    }
+    .ant-form-item {
+      margin-bottom: 12px;
+    }
+    .ant-input {
+      border-radius: 50px !important;
+      margin: -16px 0px !important;
+      height: 40px !important;
+      padding: 4px 20px;
+    }
+    .ant-form-item-children {
+      .ant-input {
+        border-radius: 50px !important;
+      }
+    }
+    .remember_me {
+      margin-top: 0px;
       font-size: 16px;
-      line-height: 30px;
-      text-align: center;
-      a {
-        color: $green-black-01;
+      font-weight: 500;
+    }
+    .form_submit {
+      display: flex;
+      justify-content: center;
+      margin-top: 10px;
+    }
+    .btn_started {
+      background: linear-gradient(90.62deg, #0b9985 -63.29%, #0b9985 100%);
+      border-radius: 50px;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 24px;
+      padding: 2px 12px;
+    }
+    .more_option {
+      margin-bottom: 30px;
+      .confirm,
+      .forgot {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 30px;
+        text-align: center;
+        a {
+          color: $green-black-01;
+        }
       }
     }
   }
-}
 </style>
