64c0f308bd1f14e63a1130385594d51fc10cc082
Nguyen Quang Tri <trinq@vutatech.vn>
2021-10-02 13:04:42 +0700

    register

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -191,221 +191,226 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
-export default {
-  data() {
-    return {
-      confirmDirty: false,
-      hasOpenLoading: false,
-      types: ["points"],
-      disabled: true,
-      loading: false,
-      active: false,
-    };
-  },
-  beforeCreate() {
-    this.form = this.$form.createForm(this, { name: "register" });
-  },
-  mounted() {
-    this.handleClickLoading("points");
-    window.myRecaptchaMethod = this.myRecaptchaMethod;
-    window.myRecaptchaExpiredMethod = this.myRecaptchaExpiredMethod;
-  },
-  computed: {
-    ...mapState("auth", ["currentUser", "error", "errorMessage"]),
-  },
-  methods: {
-    myRecaptchaMethod: function (response) {
-      console.log(response);
-      this.disabled = false;
-    },
-    myRecaptchaExpiredMethod: function () {
-      this.disabled = true;
+  import { mapState, mapActions } from "vuex";
 
-      // this is google's code verifying the user is human'
-      // that you check with google on the backend.
+  export default {
+    data() {
+      return {
+        confirmDirty: false,
+        hasOpenLoading: false,
+        types: ["points"],
+        disabled: true,
+        loading: false,
+        active: false,
+        recaptcha: ''
+      };
     },
-    handleSubmit(e) {
-      e.preventDefault();
-      this.form.validateFieldsAndScroll((err, values) => {
-        if (!err) {
-          console.log("Received values of form: ", values);
-          this.registerIntoServer(values);
-        }
-        // setTimeout(() => this.$router.push("/dashboard"), 1500);
-      });
+    beforeCreate() {
+      this.form = this.$form.createForm(this, { name: "register" });
     },
-    handleConfirmBlur(e) {
-      const value = e.target.value;
-      this.confirmDirty = this.confirmDirty || !!value;
+    mounted() {
+      this.handleClickLoading("points");
+      window.myRecaptchaMethod = this.myRecaptchaMethod;
+      window.myRecaptchaExpiredMethod = this.myRecaptchaExpiredMethod;
     },
-    compareToFirstPassword(rule, value, callback) {
-      const form = this.form;
-      if (value && value !== form.getFieldValue("password")) {
-        callback("Two passwords that you enter is inconsistent!");
-      } else {
-        callback();
-      }
+    computed: {
+      ...mapState("auth", ["currentUser", "error", "errorMessage"]),
     },
-    validateToNextPassword(rule, value, callback) {
-      const form = this.form;
-      if (value && this.confirmDirty) {
-        form.validateFields(["confirm"], { force: true });
-      }
-      callback();
-    },
-    handleClickLoading(type) {
-      const loading = this.$vs.loading({
-        type,
-        color: "#0b9985",
-      });
-      this.hasOpenLoading = true;
-      setTimeout(() => {
-        loading.close();
-        this.hasOpenLoading = false;
-      }, 1000);
-    },
-    correctCaptcha() {
-      console.log("response", 123);
-    },
-    async registerIntoServer(user) {
-      this.loading = true;
-      try {
-        await this.$store.dispatch("auth/registerIntoServer", {
-          firstname: user.first_name,
-          lastname: user.last_name,
-          phone: `+${user.prefix} ${user.phone}`,
-          email: user.email,
-          password: user.password,
-          password_confirmation: user.confirm,
-          ref_id: user.referral,
+    methods: {
+      myRecaptchaMethod: function (response) {
+        // console.log(response);
+        this.recaptcha = response;
+        this.disabled = false;
+      },
+      myRecaptchaExpiredMethod: function () {
+        this.recaptcha = '';
+        this.disabled = true;
+
+        // this is google's code verifying the user is human'
+        // that you check with google on the backend.
+      },
+      handleSubmit(e) {
+        e.preventDefault();
+        this.form.validateFieldsAndScroll((err, values) => {
+          if (!err) {
+            // console.log("Received values of form: ", values);
+            this.registerIntoServer(values);
+          }
+          // setTimeout(() => this.$router.push("/dashboard"), 1500);
         });
-        if (this.errorMessage) {
-          this.loading = false;
-          this.openNotification("top-right", "danger", "Error");
+      },
+      handleConfirmBlur(e) {
+        const value = e.target.value;
+        this.confirmDirty = this.confirmDirty || !!value;
+      },
+      compareToFirstPassword(rule, value, callback) {
+        const form = this.form;
+        if (value && value !== form.getFieldValue("password")) {
+          callback("Two passwords that you enter is inconsistent!");
         } else {
-          this.openNotification("top-right", "#0b9985", "Success");
-          this.loading = false;
-          this.$router.push("/login");
+          callback();
         }
-      } catch {
-        console.log("Error", this.error);
-      }
+      },
+      validateToNextPassword(rule, value, callback) {
+        const form = this.form;
+        if (value && this.confirmDirty) {
+          form.validateFields(["confirm"], { force: true });
+        }
+        callback();
+      },
+      handleClickLoading(type) {
+        const loading = this.$vs.loading({
+          type,
+          color: "#0b9985",
+        });
+        this.hasOpenLoading = true;
+        setTimeout(() => {
+          loading.close();
+          this.hasOpenLoading = false;
+        }, 1000);
+      },
+      correctCaptcha() {
+        console.log("response", 123);
+      },
+      async registerIntoServer(user) {
+        this.loading = true;
+        try {
+          await this.$store.dispatch("auth/registerIntoServer", {
+            firstname: user.first_name,
+            lastname: user.last_name,
+            phone: `+${user.prefix} ${user.phone}`,
+            email: user.email,
+            password: user.password,
+            password_confirmation: user.confirm,
+            ref_id: user.referral,
+            recaptcha: this.recaptcha
+          });
+          if (this.errorMessage) {
+            this.loading = false;
+            this.openNotification("top-right", "danger", "Error");
+          } else {
+            this.openNotification("top-right", "#0b9985", "Success");
+            this.loading = false;
+            this.$router.push("/login");
+          }
+        } catch {
+          console.log("Error", this.error);
+        }
+      },
+      loginWithChoise() {
+        if (!this.currentUser) {
+          this.$router.push("/login");
+        } else {
+          this.active = true;
+        }
+      },
+      acceptToLogout() {
+        window.localStorage.clear();
+        window.location.reload(true);
+        window.location.replace("login");
+      },
+      openNotification(position = null, color, title) {
+        const noti = this.$vs.notification({
+          flat: true,
+          progress: "auto",
+          color,
+          position,
+          title,
+          text: this.errorMessage ? this.errorMessage : `Register success`,
+        });
+      },
     },
-    loginWithChoise() {
-      if (!this.currentUser) {
-        this.$router.push("/login");
-      } else {
-        this.active = true;
-      }
-    },
-    acceptToLogout() {
-      window.localStorage.clear();
-      window.location.reload(true);
-      window.location.replace("login");
-    },
-    openNotification(position = null, color, title) {
-      const noti = this.$vs.notification({
-        flat: true,
-        progress: "auto",
-        color,
-        position,
-        title,
-        text: this.errorMessage ? this.errorMessage : `Register success`,
-      });
-    },
-  },
 
-  // openLoading(type, ref) {
-  //   this.$vs.loading({
-  //     target: this.$refs[ref][0],
-  //     text: type,
-  //     type
-  //   })
-  // }
-};
+    // openLoading(type, ref) {
+    //   this.$vs.loading({
+    //     target: this.$refs[ref][0],
+    //     text: type,
+    //     type
+    //   })
+    // }
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "@asset/variables.scss";
-.register {
-  width: 50%;
-  background: $background-form;
-  border: 1px solid #e4e4e4;
-  box-sizing: border-box;
-  box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.09);
-  border-radius: 20px;
-  padding: 0px 60px;
-  margin: 0 auto;
-  margin-top: 20px;
-  .title {
-    font-weight: bold;
-    font-size: 36px;
-    line-height: 24px;
-    color: $green-black-01;
-    text-align: center;
-    margin-top: 30px;
+  @import "@asset/variables.scss";
+  .register {
+    width: 50%;
+    background: $background-form;
+    border: 1px solid #e4e4e4;
+    box-sizing: border-box;
+    box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.09);
+    border-radius: 20px;
+    padding: 0px 60px;
+    margin: 0 auto;
+    margin-top: 20px;
+    .title {
+      font-weight: bold;
+      font-size: 36px;
+      line-height: 24px;
+      color: $green-black-01;
+      text-align: center;
+      margin-top: 30px;
+    }
+    .quote {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 24px;
+      text-align: center;
+      margin-top: 10px;
+      margin-bottom: 30px;
+    }
   }
-  .quote {
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 24px;
-    text-align: center;
-    margin-top: 10px;
-    margin-bottom: 30px;
-  }
-}
-.ant-form {
-  input {
-    border-radius: 50px !important;
-  }
-  .ant-form-item {
-    margin-bottom: 12px;
-  }
-  .ant-input {
-    border-radius: 50px !important;
-    margin: -16px 0px !important;
-    height: 40px !important;
-    padding: 4px 20px;
-  }
-  .ant-form-item-children {
-    .ant-input {
+  .ant-form {
+    input {
       border-radius: 50px !important;
     }
-  }
-  .privacy {
-    margin-top: 0px;
-    text-align: center;
-    font-size: 16px;
-    font-weight: 500;
-    .highlight {
-      color: $green-text-01;
-      font-weight: 700;
+    .ant-form-item {
+      margin-bottom: 12px;
+    }
+    .ant-input {
+      border-radius: 50px !important;
+      margin: -16px 0px !important;
+      height: 40px !important;
+      padding: 4px 20px;
+    }
+    .ant-form-item-children {
+      .ant-input {
+        border-radius: 50px !important;
+      }
+    }
+    .privacy {
+      margin-top: 0px;
+      text-align: center;
+      font-size: 16px;
+      font-weight: 500;
+      .highlight {
+        color: $green-text-01;
+        font-weight: 700;
+      }
+    }
+    .form_submit {
+      display: flex;
+      justify-content: center;
+      margin-top: 10px;
+    }
+    .btn_started {
+      background: linear-gradient(90.62deg, #0b9985 -63.29%, #0b9985 100%);
+      border-radius: 50px;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 24px;
+      padding: 2px 12px;
+    }
+    .login {
+      margin-top: -10px;
+      text-align: center;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 24px;
+      .highlight {
+        color: $green-text-01;
+        font-weight: 700;
+      }
     }
   }
-  .form_submit {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-  }
-  .btn_started {
-    background: linear-gradient(90.62deg, #0b9985 -63.29%, #0b9985 100%);
-    border-radius: 50px;
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 24px;
-    padding: 2px 12px;
-  }
-  .login {
-    margin-top: -10px;
-    text-align: center;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 24px;
-    .highlight {
-      color: $green-text-01;
-      font-weight: 700;
-    }
-  }
-}
 </style>
