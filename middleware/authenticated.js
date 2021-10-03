00833983a5b2957d0c40a9f170f8c7b5da28ed4e
Nguyen Quang Tri <trinq@vutatech.vn>
2021-10-03 23:09:47 +0700

    middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,7 @@
 export default function ({ store, redirect }) {
+    console.log('authen');
     // If the user is not authenticated
-    if (process.client && !localStorage.getItem("user")) {
-        return redirect("/login");
-      }
+    // if (process.client && !localStorage.getItem("user")) {
+    //     return redirect("/login");
+    //   }
   }
